aedf54ba2382cfcaffb7c5fc7f1b8606877fdf65
malmazuke <mark.feaver@gmail.com>
2013-09-03 16:40:12 +1200

    Comments

--- src/simulator/MarketSimulator.py
+++ src/simulator/MarketSimulator.py
@@ -6,7 +6,14 @@ Created on Sep 3, 2013
 
 class MarketSimulator(object):
     '''
-    classdocs
+    This trading simulator tests a trading strategy against historical data in order to evaluate 
+    how well the strategy would have performed in the past. 
+    
+    This simulator:
+    (i) simulates the price and volume of the market for each passing minute, 
+    (ii) has functionality for simulating the entry and exit of trades at appropriate points, and 
+    (iii) produces summary statistics showing how well the strategy performed overall on the historical data.
+    Any open trades are automatically closed at the end of the simulation period.
     '''
 
 
