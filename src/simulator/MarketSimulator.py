fe5d29fd26a9eb61b2180bdce30b38b404576afb
malmazuke <mark.feaver@gmail.com>
2013-09-03 16:36:48 +1200

    Init commit - created src file for simulator

---
+++ src/simulator/MarketSimulator.py
@@ -0,0 +1,17 @@
+'''
+Created on Sep 3, 2013
+
+@author: feaver
+'''
+
+class MarketSimulator(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
