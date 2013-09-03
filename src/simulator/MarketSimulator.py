e808aabdc266d399c979306f141e7a406997e175
malmazuke <mark.feaver@gmail.com>
2013-09-03 18:47:16 +1200

    Added some methods

--- src/simulator/MarketSimulator.py
+++ src/simulator/MarketSimulator.py
@@ -5,9 +5,13 @@ Created on Sep 3, 2013
 '''
 
 class MarketSimulator(object):
+    
+    _training_file = ""
+    _testing_file = ""
+    
     '''
     This trading simulator tests a trading strategy against historical data in order to evaluate 
-    how well the strategy would have performed in the past. 
+    how well the strategy would have performed in the past.
     
     This simulator:
     (i) simulates the price and volume of the market for each passing minute, 
@@ -17,8 +21,33 @@ class MarketSimulator(object):
     '''
 
 
-    def __init__(self, params):
+    def __init__(self, training_file_csv, test_file_csv = ""):
         '''
-        Constructor
-        '''
+        Constructor - takes two file locations pointing to the training and testing data. 
+        Should be in the following csv format: 
+        Date (in MM/DD/YYYY format), Time (in HHMM format), Price, and Volume.
         
+        Keyword arguments:
+        training_file_csv -- the location of the csv file containing training data.
+        test_file_csv -- the location of the csv file to test with (default none)
+        '''
+        self.set_trainingfile(training_file_csv)
+        
+        if test_file_csv:
+            self.set_testfile(test_file_csv)
+    
+    def set_trainingfile(self, training_file_csv):
+        '''
+        Set the training file. Should be in the following csv format: 
+        Date (in MM/DD/YYYY format), Time (in HHMM format), Price, and Volume.
+        '''
+        self._training_file = training_file_csv
+        
+    def set_testfile(self, test_file_csv):
+        '''
+        Set the test file. Should be in the following csv format: 
+        Date (in MM/DD/YYYY format), Time (in HHMM format), Price, and Volume.
+        '''
+        self._testing_file = test_file_csv
+        
+    
