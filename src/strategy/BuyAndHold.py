d3596706f2848cbb9493336659f56c384393735a
malmazuke <mark.feaver@gmail.com>
2013-09-14 00:05:42 -0700

    Added more schtuff for the BuyAndHold strategy

--- src/strategy/BuyAndHold.py
+++ src/strategy/BuyAndHold.py
@@ -8,12 +8,34 @@ class BuyAndHold(object):
     '''
     classdocs
     '''
-    val = None
+    _curr_position = None
+    _market = None
+    _is_holding = False
 
-
-    def __init__(self):
+    def __init__(self, market):
         '''
         Constructor
+        
+        Keyword arguments:
+        market -- the market simulator object
+        '''
+        
+        self._curr_position = Position.LONG
+        self._market = market
+        self._is_holding = True
+
+    def trade(self, market, entry):
+        '''
+        Make a decision for a specific entry on whether to go long, short, hold, or be out of the market
+        
+        Keyword arguments:
+        entry -- a single entry (i.e. minute), containing the time (datetime), price, and volume
         '''
 
-        
+class Position:
+    '''
+    Basically an enum, describing the type of trade position 
+    '''
+    OUT = 0 # Sit out of the market
+    LONG = 1 # Go long
+    SHORT = 2 # Go short
