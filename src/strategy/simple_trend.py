d361cb0ac10875a04eb16fb1fc5d9ea6f81fa00b
malmazuke <mark.feaver@gmail.com>
2013-09-14 19:46:33 -0700

    Changes to comments

--- src/strategy/simple_trend.py
+++ src/strategy/simple_trend.py
@@ -6,20 +6,23 @@ Created on Sep 14, 2013
 from strategy import Strategy
 from entities.position import Position
 
+DEFAULT_N = 1
 class SimpleTrend(Strategy):
     '''
-    SimpleTrend is a very slightly more complex strategy. The idea is that if the 
-    most recent n% price move was down, then a short position should be opened; 
-    conversely if the most recent n% move was up, then a long position should be opened. 
-    When a new position opens, the old one closes. This is a strategy that has a single parameter n 
-    that should be tuned on the training data only.
+    SimpleTrend is a very slightly more complex strategy. The idea is that if MA(n) indicator turns down 
+    (i.e. MA(t,n)<MA(t-1,n)), then a short position should be opened; conversely if the MA(t,n) indicator turns up 
+    (i.e. MA(t,n)>MA(t-1,n), then a long position should be opened. When a new position opens, the old one closes.
+    
+    This is a strategy that has a single positive integer parameter n that should be tuned on the training data only.
     '''
 
-    def __init__(self):
+    _n = 1
+    def __init__(self, n=DEFAULT_N):
         '''
         Constructor
         '''
         Strategy.__init__(self)
+        self._n = n
         
     def trade(self, index):
         '''
